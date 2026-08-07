93dead50f954b853d15d0972f39c520bcc680d61
Droffilcc <ford.eberhardy@icloud.com>
2026-08-07 11:04:50 -0500

    attempted stutter fixes
    I'm experiencing issues with the player character stuttering during gameplay. This issue does not seem to appear for Eli, so idk what could be causing it :/

--- scripts/main_camera.gd
+++ scripts/main_camera.gd
@@ -2,6 +2,7 @@ extends Camera2D
 @export var target: Node2D
 @export var SPEED: float
 
-func _process(delta: float) -> void:
-	var distance = position.distance_to(target.position)
-	position = position.move_toward(target.position, SPEED * distance * delta)
+func _physics_process(delta: float) -> void:
+	if target != null:
+		var distance = position.distance_to(target.position)
+		position = position.move_toward(target.position, SPEED * distance * delta)
