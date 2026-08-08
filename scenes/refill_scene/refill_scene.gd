dd039b6a8ef73fba59020d80eadff2c40203260f
eroza86 <emann5217@gmail.com>
2026-08-07 22:41:42 -0500

    Add refill scene with draggable bottles
    Introduces a new refill prototype with a Bottle RigidBody2D scene/script that can be picked up, dragged, and tilted, plus a RefillScene containing multiple bottle instances, floor collision, and an FPS label. Also adds a global GameManager autoload and updates the default canvas texture filter setting in project config. Includes related stage and test shell resource updates from an in-progress merge.

---
+++ scenes/refill_scene/refill_scene.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var label: Label = $Label
+
+var heldBottle: RigidBody2D
+var hasMouse: bool = false
+var SPEED: float = 10.0
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	print(heldBottle)
+	label.text = str(Engine.get_frames_per_second())
+
+func _physics_process(_delta: float) -> void:
+	pass
+
+
+func _on_bottle_is_holding() -> void:
+	pass # Replace with function body.
