d63e60eaf05c41232a9031ed19bc8404ac4e21d9
Droffilcc <ford.eberhardy@icloud.com>
2026-08-09 12:08:10 -0500

    dores

---
+++ scenes/dore.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+@export var open_pos: Vector2
+@export var close_pos: Vector2
+@export var speed: float
+var target_pos: Vector2
+
+func _ready() -> void:
+	target_pos = close_pos
+
+func open():
+	target_pos = open_pos
+func close():
+	target_pos = close_pos
+	
+func _process(delta: float) -> void:
+	position = lerp(position, target_pos, speed * delta)
---
+++ scenes/increment_dore.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+@export var open_pos: Vector2
+@export var close_pos: Vector2
+@export var speed: float
+@export var num_needed: int
+var num_on: int = 0
+var target_pos: Vector2
+
+func _ready() -> void:
+	target_pos = close_pos
+
+func increment():
+	num_on += 1
+	if num_on >= num_needed:
+		open()
+func decrement():
+	num_on -= 1
+	if num_on < num_needed:
+		close()
+
+func open():
+	target_pos = open_pos
+func close():
+	target_pos = close_pos
+	
+func _process(delta: float) -> void:
+	position = lerp(position, target_pos, speed * delta)
