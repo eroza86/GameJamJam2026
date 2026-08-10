eebe4fec74883a2c5234fb11e585250c1f40fbff
Tarin <t4r1n@tuta.io>
2026-08-10 01:33:57 -0500

    Win condition

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var death_scene: PackedScene
+@export var win_scene: PackedScene
 var player_died: bool = false
 
 func _on_player_died() -> void:
@@ -13,3 +14,10 @@ func _on_player_died() -> void:
 
 func _on_first_wall_broken() -> void:
 	$"../GlobalMusicPlayer".play_level()
+
+
+func _on_win_area_body_entered(body: Node2D) -> void:
+	if body is Player:
+		# Nice job
+		var win = win_scene.instantiate()
+		add_sibling(win)
