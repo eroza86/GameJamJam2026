48cf89829397db714e3cbb308339f76ef541a040
Tarin <t4r1n@tuta.io>
2026-08-10 00:35:04 -0500

    Health and music

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -3,9 +3,15 @@ extends Node
 
 @export var max_hp: int = 10
 @export var hurt_sound: AudioStreamPlayer2D
+@export var health_bar: ProgressBar
 @onready var hp: int = max_hp
 
 signal died
+
+func _ready() -> void:
+	if health_bar != null:
+		health_bar.max_value = max_hp
+		health_bar.value = hp
 
 # Returns remaining health
 func take_damage(dmg: int) -> int:
@@ -14,8 +20,13 @@ func take_damage(dmg: int) -> int:
 	hp = max(0, hp - dmg)
 	if hp == 0:
 		died.emit()
+	
+	if health_bar != null:
+		health_bar.value = hp
 	return hp
 
 func heal(amount: int) -> int:
 	hp = min(max_hp, hp + amount)
+	if health_bar != null:
+		health_bar.value = hp
 	return hp
