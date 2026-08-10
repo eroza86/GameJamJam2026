e446600e478a18327fc087d2bc503f146c9d4c0f
eroza86 <emann5217@gmail.com>
2026-08-09 23:43:36 -0500

    Export powder colors and add cutscene scene
    Refactors `Powder` to use an exported `color` property instead of hardcoded name-based color assignment, then wires the Stage resources to provide explicit color data and fully initialized shell resources. Also adds a new `cutscene.tscn` scaffold with a camera and removes the Stage item pickup instance to match the updated scene setup.

--- resources/powder.gd
+++ resources/powder.gd
@@ -2,19 +2,4 @@ class_name Powder
 extends Resource
 
 @export var name: String
-var color: Color
-
-func ready() -> void:
-	match name:
-		"Fire": 
-			color = Color("b2330d")
-		"Acid": 
-			color = Color("007a00")
-		"Cloud":
-			color = Color("727272")
-		"Lightning":
-			color = Color("ddb400")
-		"Lob":
-			color = Color("d300ab")
-		"Missile":
-			color = Color("f0f0f0")
+@export var color: Color
